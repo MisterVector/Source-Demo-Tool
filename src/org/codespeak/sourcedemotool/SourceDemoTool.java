6e333376a72887d43af50e385c83d23b9c6bd3f8
MisterVector <whizkid3000@hotmail.com>
2019-07-04 18:11:35 -0700

    Create "demos" and "output" folders on launch if they don't exist

--- src/org/codespeak/sourcedemotool/SourceDemoTool.java
+++ src/org/codespeak/sourcedemotool/SourceDemoTool.java
@@ -1,5 +1,6 @@
 package org.codespeak.sourcedemotool;
 
+import java.io.File;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -26,6 +27,18 @@ public class SourceDemoTool extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        File demoFolder = new File("demos");
+        
+        if (!demoFolder.exists()) {
+            demoFolder.mkdirs();
+        }
+        
+        File outputFolder = new File("output");
+        
+        if (!outputFolder.exists()) {
+            outputFolder.mkdirs();
+        }
+        
         launch(args);
     }
     
