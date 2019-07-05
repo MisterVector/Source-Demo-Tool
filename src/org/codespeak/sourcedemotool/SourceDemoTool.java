f497839c2732bc43cd0ef8632279a062b60f405a
MisterVector <whizkid3000@hotmail.com>
2019-07-04 18:05:05 -0700

    Initial commit

---
+++ src/org/codespeak/sourcedemotool/SourceDemoTool.java
@@ -0,0 +1,32 @@
+package org.codespeak.sourcedemotool;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Vector
+ */
+public class SourceDemoTool extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/org/codespeak/sourcedemotool/MainWindow.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
