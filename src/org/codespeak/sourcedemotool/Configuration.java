dc697f2cad9a92df29e2282a8a04340f185a198c
MisterVector <whizkid3000@hotmail.com>
2019-07-09 17:34:54 -0700

    Added a version variable to the configuration and appended it to the title

--- src/org/codespeak/sourcedemotool/Configuration.java
+++ src/org/codespeak/sourcedemotool/Configuration.java
@@ -6,7 +6,8 @@ package org.codespeak.sourcedemotool;
  */
 public class Configuration {
 
-    public static String PROGRAM_TITLE = "Source Demo Tool";
+    public static String PROGRAM_VERSION = "0.0.0";
+    public static String PROGRAM_TITLE = "Source Demo Tool v" + PROGRAM_VERSION;
     public static String DEMOS_FOLDER = "demos";
     public static String OUTPUT_FOLDER = "output";
     
